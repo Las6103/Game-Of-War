e0f131c2eb69ab7ad08fd59f8deb898ba4a8d057
LeonardoSilva <apx88.0@gmail.com>
2020-04-29 18:49:49 -0400

    created variables

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-//
+const suit = ["hearts", "spades", "clubs", "diamonds"];
+const rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+const value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+let deckOfCards = [];
+let playerOne = [];
+let playerTwo = [];
+
+function getDeck() {
+  for (let i = 0; i < suit.length; i++) {}
+}
