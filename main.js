746a5443794f4d02d2eb3815162d0aa9f847f0b9
LeonardoSilva <apx88.0@gmail.com>
2020-04-29 19:43:48 -0400

    working on assigning deck at random to player

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let deckOfCards = [];
 let playerOne = [];
 let playerTwo = [];
 
+// Creates Deck of 52 cards
 function getDeck() {
   for (let i = 0; i < suit.length; i++) {
     for (let j = 0; j < rank.length; j++) {
@@ -14,4 +15,21 @@ function getDeck() {
   return deckOfCards;
 }
 
-console.log(getDeck());
+// Assigns 26 cards to each player
+function assignDeck() {
+  let deck = getDeck();
+  console.log(deck);
+    deck = deck[Math.floor(Math.random() * deck.length)];
+    console.log(deck);
+  function randomDeck() {
+    playerOne = deck.splice(0, 26);
+    playerTwo = deck.splice(0, 26);
+    console.log(deck);
+  }
+  return randomDeck();
+}
+
+assignDeck();
+
+console.log(playerOne);
+console.log(playerTwo);
