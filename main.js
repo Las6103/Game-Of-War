f6b373f41fa82c3e7769ea9e49a0ada260bb3502
LeonardoSilva <apx88.0@gmail.com>
2020-04-30 21:43:33 -0400

    created first part of round function

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const suit = ["hearts", "spades", "clubs", "diamonds"];
-const rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-const value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+const rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 let deckOfCards = [];
 let randomDeck = [];
 let playerOne = [];
@@ -43,5 +42,47 @@ function gameStart() {
 }
 gameStart();
 
+// Rounds
+function rounds() {
+  // while (playerOne.length < 52 || playerTwo.length < 52) {
+    let playerOneCards = playerOne[playerOne.length - 1];
+    let playerTwoCards = playerTwo[playerTwo.length - 1];
+    console.log(playerOneCards);
+    console.log(playerTwoCards);
+    if (playerOneCards === playerTwoCards) {
+
+      // TODO: Create details function
+    } else if (playerOneCards > playerTwoCards) {
+      let playerTwoPop = playerTwo.pop();
+      playerOne.unshift(playerTwoPop);
+      // TODO: Create details function
+    } else {
+      let playerOnePop = playerOne.pop();
+      playerTwo.unshift(playerOnePop);
+      // TODO: Create details function
+    }
+  // }
+}
+rounds();
 console.log(playerOne);
 console.log(playerTwo);
+
+// compare the last numbers of array, use .length -1 to compare last element in array
+// make variables playerOneCards and playerTwoCards, compare
+// if playerOneCards > playerTwoCards then .pop playerTwoCards
+// use unshift to transfer card into playerOnceCards
+// else if
+// if playerOneCards < playerTwoCards then .pop playerOneCards
+// use unshift to transfer card into playerTwoCards
+// else
+// playerOneCards = playerTwoCards then
+//.length -4
+
+
+
+
+// greater number wins round BUT
+// if number is the same then
+// remove 3 cards from each array
+// compare the 4th card
+// greater number wins all the cards
