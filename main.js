9c1fcea95e4c939e8374e55cf23e070c61ad41e3
LeonardoSilva <apx88.0@gmail.com>
2020-05-04 14:56:47 -0400

    completed game logic

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+console.log("hello");
+// Global Variables
+const suit = ["hearts", "spades", "clubs", "diamonds"];
+const rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+const value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+let deckOfCards = [];
+let randomDeck = [];
+let playerOne = [];
+let playerTwo = [];
+let cardsInPlay = [];
+let roundWinner = "";
+let roundsplayed = "";
+let score = {
+  One: 0,
+  Two: 0,
+};
+
 class Card {
   constructor(suit, rank, value) {
     this.suit = suit;
@@ -6,13 +23,6 @@ class Card {
   }
 }
 
-const suit = ["hearts", "spades", "clubs", "diamonds"];
-const rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-const value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-let deckOfCards = [];
-let randomDeck = [];
-let playerOne = [];
-let playerTwo = [];
 // Creates Deck of 52 cards
 function createDeck() {
   for (let i = 0; i < suit.length; i++) {
@@ -24,8 +34,6 @@ function createDeck() {
   return deckOfCards;
 }
 
-console.log(deckOfCards);
-
 // Randomize the deckOfCards array
 function randomizeDeck() {
   let deck = createDeck();
@@ -33,111 +41,132 @@ function randomizeDeck() {
     let rand = deck[Math.floor(Math.random() * deck.length)];
     randomDeck.push(rand);
   }
-  return randomDeck;
 }
-randomizeDeck();
 
 // Assign Deck to players
 function assignDeck() {
-  function deckValue() {
-    playerOne = randomDeck.splice(0, 26);
-    playerTwo = randomDeck.splice(0, 26);
-  }
-  return deckValue();
+  playerOne = randomDeck.splice(0, 26);
+  playerTwo = randomDeck.splice(0, 26);
 }
-assignDeck();
-console.log(playerOne);
-console.log(playerTwo);
 
-// last card of array functino
+// Returns Value of a Card
 function topCard(arr) {
-  return arr[arr.length - 1].value;
+  if (arr[arr.length - 1]) {
+    return arr[arr.length - 1].value;
+  }
+  return 0;
 }
-
+// Returns Rank of a Card
 function log(arr) {
-  return arr[arr.length - 1].rank;
+  if (arr[arr.length - 1]) {
+    return arr[arr.length - 1].rank;
+  }
+  return "";
 }
 
-// Intial score
-let score = {
-  One: 0,
-  Two: 0,
-};
-
-// Rounds
+// Rounds of Game
 function rounds() {
-  let roundWinner = "";
-  let cardsInPlay = [];
   while (playerOne.length < 52 && playerTwo.length < 52) {
-    
     // War Situation
     if (topCard(playerOne) === topCard(playerTwo)) {
-      cardsInPlay.unshift(
-        ...playerOne.splice(playerOne.length - 3),
-        ...playerTwo.splice(playerTwo.length - 3)
-      );
+      popCardWar();
       continue;
-
-      // Player One Winner
     } else if (topCard(playerOne) > topCard(playerTwo)) {
       roundWinner = "PlayerOne";
-      cardsInPlay.unshift(playerOne.pop());
-      cardsInPlay.unshift(playerTwo.pop());
-      // Player Two Winner
+      popCardOne();
     } else {
       roundWinner = "PlayerTwo";
-      cardsInPlay.unshift(playerTwo.pop());
-      cardsInPlay.unshift(playerOne.pop());
+      popCardTwo();
     }
 
     // Winner Cards
     if (roundWinner === "PlayerOne") {
-      console.log("player one: ", playerOne);
-      console.log("player two: ", playerTwo);
-      console.log(cardsInPlay);
-      playerOne.unshift(...cardsInPlay);
-      console.log(
-        `Player one got away with ${details(
-          cardsInPlay
-        )} | Player One plays a ${log(playerOne)} | Player Two plays a ${log(
-          playerTwo
-        )}`
-      );
-      
-      cardsInPlay = [];
-      score.One += 1;
+      playerOneWinner();
     } else {
-      console.log("player one: ", playerOne);
-      console.log("player two: ", playerTwo);
-      console.log(cardsInPlay);
-      playerTwo.unshift(...cardsInPlay);
-      console.log(
-        `Player Two snatched ${details(cardsInPlay)} | Player One plays a ${log(
-          playerOne
-        )} | Player Two plays a ${log(playerTwo)}`
-      );
-      
-      cardsInPlay = [];
-      score.Two += 1;
+      playerTwoWinner();
     }
   }
+  console.log("Game Over " + roundWinner + " wins");
+  gameReview();
 }
-rounds();
+
+// Player One Winner Pop
+function popCardOne() {
+  if (topCard(playerOne)) {
+    cardsInPlay.unshift(playerOne.pop());
+  }
+  if (topCard(playerTwo)) {
+    cardsInPlay.unshift(playerTwo.pop());
+  }
+}
+
+// Player Two Winner Pop
+function popCardTwo() {
+  if (topCard(playerTwo)) {
+    cardsInPlay.unshift(playerTwo.pop());
+  }
+
+  if (topCard(playerOne)) {
+    cardsInPlay.unshift(playerOne.pop());
+  }
+}
+
+// War Time Pop
+function popCardWar() {
+  cardsInPlay.unshift(
+    ...playerOne.splice(playerOne.length - 3),
+    ...playerTwo.splice(playerTwo.length - 3)
+  );
+  console.log(
+    `Whose gonna win these cards! Player One places a ${log(
+      playerOne
+    )} | Player Two places a ${log(playerTwo)}`
+  );
+}
+
+// Player One Wins
+function playerOneWinner() {
+  playerOne.unshift(...cardsInPlay);
+  roundsplayed++;
+  console.log(
+    `Player one got away with ${details(
+      cardsInPlay
+    )}  | End of round ${roundsplayed} | Player One plays a ${log(
+      playerOne
+    )} | Player Two plays a ${log(playerTwo)}`
+  );
+  cardsInPlay = [];
+  score.One += 1;
+}
+
+// Player Two Wins
+function playerTwoWinner() {
+  playerTwo.unshift(...cardsInPlay);
+  roundsplayed++;
+  console.log(
+    `Player Two snatched ${details(
+      cardsInPlay
+    )} | End of round ${roundsplayed} | Player One plays a ${log(
+      playerOne
+    )} | Player Two plays a ${log(playerTwo)}`
+  );
+  cardsInPlay = [];
+  score.Two += 1;
+}
 
 // Details of Round
 function details(cardsInPlay) {
-  let cardsInPlayRank = [];
-  if (cardsInPlay.length > 0) {
-    for (let i = 0; i < cardsInPlay.length; i++) {
-      cardsInPlayRank.push(cardsInPlay[i].rank);
-    }
-    return cardsInPlayRank;
-  } else {
-    console.log("Game Over");
-  }
+  return cardsInPlay.map((card) => card.rank);
 }
 
-// WAR!!!!
-console.log("score: ", score);
-console.log("player one: ", playerOne);
-console.log("player two: ", playerTwo);
+function gameReview() {
+  console.log("Score: ", score);
+  console.log(
+    "Player One Cards: ",
+    playerOne.map((card) => card.rank)
+  );
+  console.log(
+    "Player Two Cards: ",
+    playerTwo.map((card) => card.rank)
+  );
+}
